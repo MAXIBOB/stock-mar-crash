236baedfdd1684abdcdf8ed86cc7c260bff18c12
MAXIBOB <maxim@makovey.net>
2018-09-08 12:39:57 -0700

    Basic layout

--- stock-mar-crash.py
+++ stock-mar-crash.py
@@ -1,1 +1,58 @@
-print "Welcome to choose your own addventure!"
+def choose_your_own_adventure():
+  print "Welcome to choose your own adventure!"
+  print
+  print "xyz"
+  answer = raw_input("What do you do? ")
+  if answer == "a":
+    print
+    print "abc"
+    answer = raw_input("What do you do? ")
+    if answer == "b":
+      print
+      print "def"
+      answer = raw_input("What do you do?")
+      if answer == "c":
+        print
+        print "ghi"
+        answer = raw_input("What do you do?")
+        if answer == "d":
+          print
+          print "Congratulations!"
+          answer = raw_input("Play again? ")
+          if answer.lower() == "yes" or answer.lower == "y":
+            choose_your_own_adventure()
+          else:
+            print "Good-bye"
+        else:
+          print
+          print "Too bad"
+          answer = raw_input("Play again? ")
+          if answer.lower() == "yes" or answer.lower() == "y":
+            choose_your_own_adventure()
+          else:
+            print "Good-bye"
+      else:
+        print
+        print "Too bad"
+        answer = raw_input("Play again? ")
+        if answer.lower() == "yes" or answer.lower() == "y":
+          choose_your_own_adventure()
+        else:
+          print "Good-bye"
+    else:
+      print
+      print "Too bad"
+      answer = raw_input("Play again? ")
+      if answer.lower() == "yes" or answer.lower() == "y":
+        choose_your_own_adventure()
+      else:
+        print "Good-bye"
+  else:
+    print
+    print "Too bad"
+    answer = raw_input("Play again? ")
+    if answer.lower() == "yes" or answer.lower() == "y":
+      choose_your_own_adventure()
+    else:
+      print "Good-bye"
+choose_your_own_adventure()
